7935d8209a1eb64702020378076849ac9d03a5a4
hventayen <55516542+hventayen@users.noreply.github.com>
2020-12-09 11:19:25 -0800

    Add files via upload

---
+++ IntegerLinkedList (3).cpp
@@ -0,0 +1,21 @@
+// ADD ANSWER TO THIS FILE
+
+#include "IntegerLinkedList.h"
+
+// int IntegerLinkedList::count(int compare) // COMPLETE THIS FOR PROBLEM 2
+
+int IntegerLinkedList::countRecurse (SNode *ptr, int compare) {
+  // COMPLETE THIS FOR PROBLEM 3
+}
+
+void IntegerLinkedList::addFront(int x) {
+  SNode *tmp = head;
+	head = new SNode;
+	head->next = tmp;
+	head->elem = x;
+}
+
+// recursion helper function called from main for PROBLEM 3
+int IntegerLinkedList::countRecurseHelper (int compare) {
+  return countRecurse(head, compare);
+}
